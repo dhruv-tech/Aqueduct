1a460abe50e575e7a2482221d20768696090a30a
LianD <Lian.Duan@Alumni.ubc.ca>
2020-01-11 14:43:43 -0800

    Added Stocks

---
+++ scrapper/stocks.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getStocks: function(){
+        //function that requests stock JSON file and then parses it returning a string
+        parsestockJSON(null);
+    },
+
+    getValue: function(){
+
+    },
+};
+
+function parsestockJSON(stockJson){
+    return null;
+}
